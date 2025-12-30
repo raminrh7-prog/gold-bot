1839704eddcdeed3ed78c777e9ddcb46402c57a2
raminrh7-prog <raminrh7@gmail.com>
2025-12-31 03:01:38 +0330

    Update and rename test.py to test_send.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from telegram import Bot
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-bot = Bot(token=BOT_TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="✅ تست موفق ارسال پیام")
